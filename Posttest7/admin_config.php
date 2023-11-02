6a5d7fecf6dd2811d062620a5e086d9f53d0c24f
aesxiety <110841723+aesxiety@users.noreply.github.com>
2023-11-03 03:18:04 +0800

    Features Login,Regist and Service Booking

---
+++ Posttest7/admin_config.php
@@ -0,0 +1,6 @@
+<?php
+    $admins = [
+        ['username' => 'reza', 'password' => password_hash('123', PASSWORD_DEFAULT)],
+        ['username' => 'ariyadi', 'password' => password_hash('234', PASSWORD_DEFAULT)]
+    ];
+?>
